286269bb7944c829b1b9d2af6554a7d0496bbe5f
Leonard Roll <leonard_roll@yahoo.co.uk>
2016-05-27 20:02:03 +0100

    Finished the Initial Layout
    Only uses fake data at the moment

--- Sunshine/app/src/main/java/com/roll/leonard/sunshine/MainActivityFragment.java
+++ Sunshine/app/src/main/java/com/roll/leonard/sunshine/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +23,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
         String[] forecastArray = {
                 "Today - Sunny - 72/63",
                 "Tomorrow - Rainy - 45/75",
@@ -31,6 +34,10 @@ public class MainActivityFragment extends Fragment {
                 "Sunday - Rainy but you'll be LIT - 48/26"
         };
         List<String> forecast = new ArrayList<String>(Arrays.asList(forecastArray));
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        //Adapter
+        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecast);
+        ListView List = (ListView) rootView.findViewById(R.id.listView_forecast);
+        List.setAdapter(mForecastAdapter);
+        return rootView;
     }
 }
